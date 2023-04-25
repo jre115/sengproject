d7062330d8377d600ab496126000bacf49254f71
Jordan Redfern <jre115@uclive.ac.nz>
2023-04-25 16:15:41 +1200

    Created name exception class for name inputs

---
+++ src/project/NameException.java
@@ -0,0 +1,14 @@
+package project;
+
+public class NameException extends IllegalArgumentException{
+	
+	public NameException() {
+		super();
+	}
+	
+	public NameException(String message) {
+		super(message);
+	}
+	
+
+}
