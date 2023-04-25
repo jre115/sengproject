db5282dfd7116e29e9614732ea077cb74d66651c
campell <cpe81@uclive.ac.nz>
2023-04-25 20:08:53 +1200

    add energy drink class inherits Item super class

---
+++ src/project/EnergyDrink.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package project;
+
+/**
+ * @author cpell
+ *
+ */
+public class EnergyDrink extends Items{
+
+	public EnergyDrink() {
+		super("EnergyDrink", "An energy drink that boosts attack", 5, 10, 0);
+		
+	}
+	
+	
+	
+		
+	
+
+}
