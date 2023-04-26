87b88469c0964c75dab2597693ed654ec216b88f
Jordan Redfern <jre115@uclive.ac.nz>
2023-04-26 13:55:44 +1200

    Updated purchasable to include constructor, getters, setters (also corrected spelling)

---
+++ src/project/Purchasable.java
@@ -0,0 +1,45 @@
+package project;
+
+public class Purchasable {
+	
+	private int contractPrice;
+	private int sellbackPrice;
+	private String description;
+	
+    public Purchasable(int contractPrice, int sellbackPrice, String description) {
+        this.contractPrice = contractPrice;
+        this.sellbackPrice = sellbackPrice;
+        this.description = description;
+    }
+    
+    public int getContractPrice() {
+        return contractPrice;
+    }
+    
+    public void setContractPrice(int contractPrice) {
+        this.contractPrice = contractPrice;
+    }
+    
+    public int getSellBackPrice() {
+        return sellbackPrice;
+    }
+    
+    public void setSellBackPrice(int sellBackPrice) {
+        this.sellbackPrice = sellBackPrice;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+    
+    
+
+}
+
+
+
