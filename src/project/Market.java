527a91674c25bf2b202dabfdccab85bf357e1e54
Cam pellett <cpe81@uclive.ac.nz>
2023-05-05 21:44:05 +1200

    added Items to Market class, added a a way to select 3 random items

--- src/project/Market.java
+++ src/project/Market.java
@@ -2,8 +2,38 @@ package project;
 
 import java.util.ArrayList;
 import java.util.Random;
-
 public class Market {
+	// Creates new items
+	public Item Broom = new Item("Nimbus2023", 5, 5, 1000, 200, "The newest model of broom currently avialble on the market");
+	public Item LuckPotion = new Item("LiquidLuck", 7, 7, 1500, 200, "A potion that increaes your Luck");
+	public Item Gloves = new Item("Enchanted Gloves", 7, 3, 1000, 300, "Gloves that a blessed with stregnth magic giving you mighty throws");
+	public Item Broom2 = new Item("SkySoarer9000", 14, 0, 2000, 1200, "A covted broom with unmatched speed");
+	public Item charm = new Item("Anti Cheat Charm", 0, 8, 800, 100, "A magical spell that prevents the opposing team from using any underhanded tactics, such as jinxes or hexes, to try and score");
+	public Item TimeTurner = new Item("TimeTurner", 0, 15, 2000, 1000, "A magical device that allows the goalie to go back in time and correct any mistakes they may have made during the game");
+	public Item fiveSensePotion = new Item("Five Sense Potion", 6, 6, 1200, 3000, "A potion that hightens the 5 senses");
+	
+	/// adds items to an array list then randomly selects 3 items to pit in a new array list
+	public ArrayList<Item> itemShop() {
+	    ArrayList<Item> shopItems = new ArrayList<Item>();
+	    shopItems.add(Broom);
+	    shopItems.add(LuckPotion);
+	    shopItems.add(Gloves);
+	    shopItems.add(Broom2);
+	    shopItems.add(charm);
+	    shopItems.add(TimeTurner);
+	    shopItems.add(fiveSensePotion);
+
+	    ArrayList<Item> selectedItems = new ArrayList<Item>();
+	    Random random = new Random();
+	    while (selectedItems.size() < 3) {
+	        int index = random.nextInt(shopItems.size());
+	        if (!selectedItems.contains(shopItems.get(index))) {
+	            selectedItems.add(shopItems.get(index));
+	        }
+	    }
+	    return selectedItems;
+	}
+	
 	/// generates random number between 3,5
 	Random R = new Random();
 	int min = 3;
@@ -29,3 +59,4 @@ public class Market {
 	
 
 }}
+
