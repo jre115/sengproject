1cdf98cc6b83b30a7a40256d5c646c9b0e7f022f
campell <cpe81@uclive.ac.nz>
2023-05-16 19:11:49 +1200

    inventory full excpetion class

--- src/project/InventoryFullException.java
+++ src/project/InventoryFullException.java
@@ -1,5 +1,7 @@
 package project;
 
-public class InventoryFullException {
-
+public class InventoryFullException extends Exception {
+    public InventoryFullException() {
+        super("Inventory is full. Cannot add item.");
+    }
 }
