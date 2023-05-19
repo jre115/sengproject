a80af6077ea32876f6781d30d66cc57f37bb05ce
campell <cpe81@uclive.ac.nz>
2023-05-19 12:23:51 +1200

    moved preform random events method to randomevent class

--- src/project/RandomEvents.java
+++ src/project/RandomEvents.java
@@ -1,89 +1,108 @@
-package project;
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
+
+
 public class RandomEvents {
-	
-	public Athlete athleteQuits(Team team) {
-		
-		ArrayList<Athlete> teamList = team.getTeamList();
-		int teamSize = teamList.size();
-		Random random = new Random();
+
+    public Athlete athleteQuits(Team team) {
+        ArrayList<Athlete> teamList = team.getTeamList();
+        int teamSize = teamList.size();
+        Random random = new Random();
         Athlete athlete = teamList.get(random.nextInt(teamSize));
+        
         if (athlete.getPosition().equals("Injured")) {
-        	team.removeFromTeam(athlete);
-        	
-            
+            team.removeFromTeam(athlete);
         } else {
-            
-            
+            // Handle the case when the athlete does not quit
         }
-		return athlete;
+        
+        return athlete;
     }
-	
-	
-	 public Athlete athleteJoins(Team team) {
-		    ArrayList<Athlete> teamList = team.getTeamList();
-		    ArrayList<Athlete> reservesList = team.getReservesList();
-		    Athlete athlete = new Athlete();
 
-		    if (teamList.size() < 5) {
-		        teamList.add(athlete);
-		    } else if (reservesList.size() < 5) {
-		        reservesList.add(athlete);
-		    } else {
-		        // Both team list and reserves list are full, do nothing
-		        return null;
-		    }
-		    return athlete;
-		}
-		
-		
-	
-	public Athlete increaseRandomPlayerStat(Team team) {
-	    
-	    ArrayList<Athlete> teamList = team.getTeamList();
-	    int teamSize = teamList.size();
-	    if (teamSize > 0) {
-	        Random random = new Random();
-	        Athlete randomPlayer = teamList.get(random.nextInt(teamSize));
+    public Athlete athleteJoins(Team team) {
+        ArrayList<Athlete> teamList = team.getTeamList();
+        ArrayList<Athlete> reservesList = team.getReservesList();
+        Athlete athlete = new Athlete();
 
-	        // Determine which statistic to increase (offensive, defensive, or stamina)
-	        int statToIncrease = random.nextInt(3); // 0 - Offensive, 1 - Defensive, 2 - Stamina
+        if (teamList.size() < 5) {
+            teamList.add(athlete);
+        } else if (reservesList.size() < 5) {
+            reservesList.add(athlete);
+        } else {
+            // Both team list and reserves list are full, do nothing
+            return null;
+        }
+        
+        return athlete;
+    }
 
-	        
-	        switch (statToIncrease) {
-	            case 0: // Offensive
-	                int currentOffensive = randomPlayer.getOffensive();
-	                int increasedOffensive = currentOffensive + random.nextInt(5) + 5; // Increase by 5-15
-	                randomPlayer.setOffensive(increasedOffensive);
-	                break;
-	            case 1: // Defensive
-	                int currentDefensive = randomPlayer.getDefensive();
-	                int increasedDefensive = currentDefensive + random.nextInt(5) + 5; // Increase by 5-15
-	                randomPlayer.setDefensive(increasedDefensive);
-	                break;
-	            case 2: // Stamina
-	                int currentStamina = randomPlayer.getStamina();
-	                int increasedStamina = currentStamina + random.nextInt(5) + 5; // Increase by 5-15
-	                randomPlayer.setStamina(increasedStamina);
-	                break;
-	        }
-	        return randomPlayer;
+    public Athlete increaseRandomPlayerStat(Team team) {
+        ArrayList<Athlete> teamList = team.getTeamList();
+        int teamSize = teamList.size();
+        
+        if (teamSize > 0) {
+            Random random = new Random();
+            Athlete randomPlayer = teamList.get(random.nextInt(teamSize));
 
-	       
-	        
-	    } else {
-	        return null;
-	    }
-	    
-	   
-	    
-	    
-	    
-	    
-	    
-	}
-	
+            // Determine which statistic to increase (offensive, defensive, or stamina)
+            int statToIncrease = random.nextInt(3); // 0 - Offensive, 1 - Defensive, 2 - Stamina
+
+            switch (statToIncrease) {
+                case 0: // Offensive
+                    int currentOffensive = randomPlayer.getOffensive();
+                    int increasedOffensive = currentOffensive + random.nextInt(5) + 5; // Increase by 5-15
+                    randomPlayer.setOffensive(increasedOffensive);
+                    break;
+                case 1: // Defensive
+                    int currentDefensive = randomPlayer.getDefensive();
+                    int increasedDefensive = currentDefensive + random.nextInt(5) + 5; // Increase by 5-15
+                    randomPlayer.setDefensive(increasedDefensive);
+                    break;
+                case 2: // Stamina
+                    int currentStamina = randomPlayer.getStamina();
+                    int increasedStamina = currentStamina + random.nextInt(5) + 5; // Increase by 5-15
+                    randomPlayer.setStamina(increasedStamina);
+                    break;
+            }
+            
+            return randomPlayer;
+        } else {
+            return null;
+        }
+    }
+    
+    public Map<String, Object> performRandomEvent(Team team) {
+        Random random = new Random();
+        int eventChance = random.nextInt(100);
+
+        Map<String, Object> eventDetails = new HashMap<>();
+
+        if (eventChance < 1) { // 1% chance for athlete quits
+            Athlete athleteToQuit = athleteQuits(team);
+            if (athleteToQuit != null) {
+                eventDetails.put("eventType", "athleteQuits");
+                eventDetails.put("athlete", athleteToQuit);
+            }
+        } else if (eventChance < 5) { // 4% chance for athlete joins
+            Athlete newAthlete = athleteJoins(team);
+            eventDetails.put("eventType", "athleteJoins");
+            eventDetails.put("athlete", newAthlete);
+        } else if (eventChance < 15) { // 10% chance for increasing a random player's stat
+            Athlete athlete = increaseRandomPlayerStat(team);
+            eventDetails.put("eventType", "increaseStat");
+            eventDetails.put("athlete", athlete);
+        } else {
+            eventDetails.put("eventType", "rest");
+        }
+
+        return eventDetails;
+    }
 }
+	
+
+
+
+
