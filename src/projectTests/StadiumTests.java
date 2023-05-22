1a930d7fdd07ab4aef6ce7e9059a7c4127047cc0
Jordan Redfern <jre115@uclive.ac.nz>
2023-05-22 19:25:29 +1200

    updated tests for stadium to match stadium updates

--- src/projectTests/StadiumTests.java
+++ src/projectTests/StadiumTests.java
@@ -2,78 +2,104 @@ package projectTests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 import java.util.ArrayList;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import project.Athlete;
 import project.Match;
 import project.Stadium;
+import project.Team;
+
+/**
+ * Tests for the Stadium class
+ * 
+ * @author Jordan Redfern 
+ * @version 1.1, May 2023.
+ */
 class StadiumTests {
 	
+	static Team playerTeam;
+	
+	@BeforeAll
+	public static void createTeam() {
+		Team team = new Team();
+		ArrayList<Athlete> generatedAthletes = team.getInitialAthletes();
+		for (Athlete athlete : generatedAthletes) {
+			team.addToTeam(athlete, null);
+		}
+		
+		assertEquals(team.getTeamList().size(), 4);
+		
+		playerTeam = team;
+	}
+	
+	
 	/**
-	 * Tests that an easy opposition generated has money and points within the specified range,
-	 * and that the generated opposition team is full with 2 attackers and 2 defenders.
+	 * Tests that generating an easy opposition team is full with 2 attackers and 2 defenders.
 	 */
 	@Test
 	public void testGenerateEasyOpposition() {
         Stadium stadium = new Stadium("Normal");
+        Team team = stadium.generateEasyOpposition();
+        
+        // 4 players in the generated opposition team
+        assertEquals(4, team.getTeamList().size());
+        
+        int defensivePlayers = 0;
+        int attackingPlayers = 0;
+        for (Athlete athlete : team.getTeamList()) {
+        	if (athlete.getPosition().equals("Attacker")) {
+        		attackingPlayers += 1;
+        	} else if (athlete.getPosition().equals("Defender")) {
+        		defensivePlayers += 1;
+        	}
+        }
+        
+        assertEquals(attackingPlayers, 2);
+        assertEquals(defensivePlayers, 2);
+	}
+	
+	/**
+	 * Tests that the has money and points fall within the specified range when generating an easy match with an easy difficulty string set for the game
+	 */
+	@Test
+	public void testGenerateEasyMatch() {
+        Stadium stadium = new Stadium("Normal");
         double difficultyMultiplier = 1.15;
         int currentWeek = 1;
         		
-        Match match = stadium.generateEasyOpposition();
-        
-        // 4 players in the generated opposition team
-        assertEquals(4, match.getOppositionTeam().getTeamList().size());
+        Match match = stadium.generateEasyMatch();
 
         double expectedMinMoneyVal = 500 * difficultyMultiplier * (0.95 + currentWeek * 0.01);
         double expectedMaxMoneyVal = 1500 * difficultyMultiplier * (0.95 + currentWeek * 0.01);
-        assertTrue(match.getMoney() >= expectedMinMoneyVal && match.getMoney() <= expectedMaxMoneyVal);
+        
+        int money = match.getMoney();
+        assertTrue(money >= (int) expectedMinMoneyVal && match.getMoney() <= (int) expectedMaxMoneyVal);
+        assertFalse(money < (int) expectedMinMoneyVal);
+        assertFalse(money > (int) expectedMaxMoneyVal);
 
         
         int expectedOppositionTeamScore = (int) (match.getOppositionTeam().getTeamScore() / 1.7);
         assertEquals(expectedOppositionTeamScore, match.getPointsValue());
-        
-        int defensivePlayers = 0;
-        int attackingPlayers = 0;
-        for (Athlete athlete : match.getOppositionTeam().getTeamList()) {
-        	if (athlete.getPosition().equals("Attacker")) {
-        		attackingPlayers += 1;
-        	} else if (athlete.getPosition().equals("Defender")) {
-        		defensivePlayers += 1;
-        	}
-        }
-        
-        assertEquals(attackingPlayers, 2);
-        assertEquals(defensivePlayers, 2);
-        
     }
 	
 	/**
-	 * Tests that a medium opposition generated has money and points within the specified range,
-	 * and that the generated opposition team is full with 2 attackers and 2 defenders.
+	 * Tests that a generated medium opposition team is full with 2 attackers and 2 defenders.
 	 */
 	@Test
 	public void testGenerateMediumOpposition() {
-        Stadium stadium = new Stadium("Hard");
-		int difficultyMultiplier = 1;
-		int difficultyDivider = 2;
-        int currentWeek = 2;
-        
-        Match match = stadium.generateMediumOpposition();
+        Stadium stadium = new Stadium("Normal");
+        Team team = stadium.generateMediumOpposition();
         
         // 4 players in the generated opposition team
-        assertEquals(4, match.getOppositionTeam().getTeamList().size());
-
-        double expectedMinMoneyVal = 1000 * difficultyMultiplier * (0.95 + currentWeek * 0.01);
-        double expectedMaxMoneyVal = 2000 * difficultyMultiplier * (0.95 + currentWeek * 0.01);
-        assertTrue(match.getMoney() >= expectedMinMoneyVal && match.getMoney() <= expectedMaxMoneyVal);
-        
-        int expectedOppositionTeamScore = (int) (match.getOppositionTeam().getTeamScore() / difficultyDivider);
-        assertEquals(expectedOppositionTeamScore, match.getPointsValue());
+        assertEquals(4, team.getTeamList().size());
         
         int defensivePlayers = 0;
         int attackingPlayers = 0;
-        for (Athlete athlete : match.getOppositionTeam().getTeamList()) {
+        for (Athlete athlete : team.getTeamList()) {
         	if (athlete.getPosition().equals("Attacker")) {
         		attackingPlayers += 1;
         	} else if (athlete.getPosition().equals("Defender")) {
@@ -83,45 +109,80 @@ class StadiumTests {
         
         assertEquals(attackingPlayers, 2);
         assertEquals(defensivePlayers, 2);
+	}
+	
+	/**
+	 * Tests that the has money and points fall within the specified range when generating a medium match with the hard difficulty string
+	 */
+	@Test
+	public void testGenerateMediumMatch() {
+        Stadium stadium = new Stadium("Hard");
+		int difficultyMultiplier = 1;
+		int difficultyDivider = 2;
+        int currentWeek = 1;
+        		
+        Match match = stadium.generateMediumMatch();
         
+        double expectedMinMoneyVal = 1000 * difficultyMultiplier * (0.95 + currentWeek * 0.01);
+        double expectedMaxMoneyVal = 2000 * difficultyMultiplier * (0.95 + currentWeek * 0.01);
+        assertTrue(match.getMoney() >= (int) expectedMinMoneyVal && match.getMoney() <= (int) expectedMaxMoneyVal);
+        assertFalse(match.getMoney() < (int) expectedMinMoneyVal);
+        assertFalse(match.getMoney() > (int) expectedMaxMoneyVal);
+
+        
+        int expectedOppositionTeamScore = (int) (match.getOppositionTeam().getTeamScore() / difficultyDivider);
+        assertEquals(expectedOppositionTeamScore, match.getPointsValue());
     }
 	
 	/**
-	 * Tests that a difficult opposition match generated has money and points within the specified range,
-	 * and that the generated opposition team is full with 2 attackers and 2 defenders.
+	 * Tests that generated difficult opposition team is full with 2 attackers and 2 defenders.
 	 */
 	@Test
 	public void testGenerateDifficultOpposition() {
+        Stadium stadium = new Stadium("Hard");
+        Team team = stadium.generateDifficultOpposition();
+        
+        // 4 players in the generated opposition team
+        assertEquals(4, team.getTeamList().size());
+        
+        int defensivePlayers = 0;
+        int attackingPlayers = 0;
+        for (Athlete athlete : team.getTeamList()) {
+        	if (athlete.getPosition().equals("Attacker")) {
+        		attackingPlayers += 1;
+        	} else if (athlete.getPosition().equals("Defender")) {
+        		defensivePlayers += 1;
+        	}
+        }
+        
+        assertEquals(attackingPlayers, 2);
+        assertEquals(defensivePlayers, 2);
+	}
+	
+	/**
+	 * Tests that the has money and points fall within the specified range when generating a medium match with the normal difficulty string
+	 */
+	@Test
+	public void testGenerateDifficultMatch() {
         Stadium stadium = new Stadium("Normal");
 		double difficultyMultiplier = 1.15;
 		double difficultyDivider = 1.7;
         int currentWeek = 2;
+        stadium.setStadium(playerTeam, currentWeek);
+        		
+        Match match = stadium.generateDifficultMatch();
         
-        Match match = stadium.generateDifficultOpposition();
-        
-        // 4 players in the generated opposition team
-        assertEquals(4, match.getOppositionTeam().getTeamList().size());
-
         double expectedMinMoneyVal = 1500 * difficultyMultiplier * (0.95 + currentWeek * 0.01);
         double expectedMaxMoneyVal = 2500 * difficultyMultiplier * (0.95 + currentWeek * 0.01);
-        assertTrue(match.getMoney() >= expectedMinMoneyVal && match.getMoney() <= expectedMaxMoneyVal);
+        assertTrue(match.getMoney() >= (int) expectedMinMoneyVal && match.getMoney() <= (int) expectedMaxMoneyVal);
+        assertFalse(match.getMoney() < (int) expectedMinMoneyVal);
+        assertFalse(match.getMoney() > (int) expectedMaxMoneyVal);
+
         
         int expectedOppositionTeamScore = (int) (match.getOppositionTeam().getTeamScore() / difficultyDivider);
         assertEquals(expectedOppositionTeamScore, match.getPointsValue());
-        
-        int defensivePlayers = 0;
-        int attackingPlayers = 0;
-        for (Athlete athlete : match.getOppositionTeam().getTeamList()) {
-        	if (athlete.getPosition().equals("Attacker")) {
-        		attackingPlayers += 1;
-        	} else if (athlete.getPosition().equals("Defender")) {
-        		defensivePlayers += 1;
-        	}
-        }
-        
-        assertEquals(attackingPlayers, 2);
-        assertEquals(defensivePlayers, 2);
     }
+
 	
 	/**
 	 * Tests that when a match is played, the match is removed from the stadium list of matches available for the week
